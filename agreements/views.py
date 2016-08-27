08ce2613646cb0605081822ce64751dd111dd7a9
krocozabr <krocozabr@gmail.com>
2016-08-27 10:04:54 +0300

    fix filters for request

--- agreements/views.py
+++ agreements/views.py
@@ -1,6 +1,6 @@
 from datetime import date
 from agreements.models import Agreement
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
 
@@ -8,18 +8,19 @@ def json_view(request):
     country = request.GET.get('country')
     negotiator = request.GET.get('negotiator')
     company = request.GET.get('company')
-
     last_5_years = range(date.today().year-5, date.today().year+1)
     query = Agreement.objects.filter(end_date__year__in=last_5_years)
-    if country:
-        query = query.filter(company__country__id__in=list(country))
-    if negotiator:
-        query = query.filter(negotiator__id__in=list(negotiator))
-    if company:
-        query = query.filter(company__id__in=list(company))
+    try:
+        # if country:
+        #     query = query.filter(company__country__id__in=[int(i) if i.isdigit() else i for i in country.split(',')])
+        if negotiator:
+            query = query.filter(negotiator__id__in=[int(i) if i.isdigit() else i for i in negotiator.split(',')])
+        if company:
+            query = query.filter(company__id__in=[int(i) if i.isdigit() else i for i in company.split(',')])
+    except ValueError as e:
+        return HttpResponseBadRequest("Please input correct data: " + e.message)
 
     result = dict()
-
     for year in last_5_years:
         year_query = query.filter(end_date__year=year)
         month_list = []
